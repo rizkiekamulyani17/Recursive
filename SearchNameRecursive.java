b9c689fed1a1eea63abbf6d84fe35a8d0177d28d
Rizki Eka Mulyani <92661180+rizkiekamulyani17@users.noreply.github.com>
2022-03-16 16:40:38 +0700

    Add files via upload

---
+++ SearchNameRecursive.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class SearchNameRecursive {
+    static int index=0;
+    static String status;
+    static void FindName(String searchtext,String [] data){
+        if(index<data.length ){
+            if(data[index].equals(searchtext)){
+                status="Data Match";
+                System.out.println(searchtext+" is on position index = "+index + "of array " + data[index]+""+status );
+                System.exit(0);
+            }else{
+                if (index < 6){
+                    index++;
+                    status="Not Match";
+                    System.out.println(searchtext+" "+data[index]+" "+status);
+                }
+            }
+        }
+
+    }
+
+    static String inputName(){
+        try{
+           String name;
+           Scanner nameInput = new Scanner(System.in);
+           System.out.print("Masukan Nama Yang Dicari =");
+            name = nameInput.nextLine();
+            nameInput.close();
+           return name;
+        }catch (Exception e){
+       }
+       return null;
+   }
+
+    public static void main(String[] args) {
+        String[] people = {"Rudi","Agus","Putri","Ika","Rizki","Summacher"};
+        String nameSearch = inputName() ;
+        FindName(nameSearch,people);
+    }
+}
